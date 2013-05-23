162322ade9f6998f4986e332d54beb0ea423a810
Juzer Ali <er.juzerali@gmail.com>
2013-05-24 03:20:51 +0530

    First working version

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require("./lib/Router");
+module.exports = require("./lib/Router/index");
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,40 @@
 var express = require('express')
+  , router = require("../index")
   , request = require('supertest')
   , assert = require('assert')
   , methods = require('methods');
   
 describe("Router", function(){
-    it("should work", function(done){
+  describe('Router header matcher', function(){
+    it("should match header value", function(done){
         var app = express();
-        
-        app.get("/?x=y", function(req, res){
-            assert(req.params.x === "y");
-            res.status(200);
+        router.extend(app);
+
+        app.get("/", "Accept: application/json", function(req, res){
+            res.status(200).end();
         });
         
         request(app)
-            .get("/?x=y")
+            .get("/")
+            .set('Accept', 'application/json')
             .expect(200, done);
-    });    
+    });
+  });
+
+    describe('Router param matcher', function(){
+      it('should match parameters', function(done){
+        var app = express();
+        router.extend(app);
+
+        app.get("/?x=a&z=c", function(req, res){debugger
+            assert(req.param("x") === "a")
+            assert(req.param("z") === "c")
+            res.status(200).end();
+        });
+        
+        request(app)
+            .get("/?x=a&y=b&z=c")
+            .expect(200, done);
+      });  
+    });  
 });
