66dfd6b2d20cbd2ed4f4467858d54db44401b0ed
Juzer Ali <er.juzerali@gmail.com>
2013-06-15 17:35:30 +0530

    Better test cases

--- test/header.js
+++ test/header.js
@@ -2,14 +2,16 @@ var express = require('express')
   , router = require("../index")
   , request = require('supertest')
   , chai = require("chai")
-  , should = chai.should();
+  , should = chai.should()
+  , methods = require("methods");
   
 var app = express();
 router.extend(app);
 
 describe('Header', function(){
-  it("Two headers", function(done){
-      app.get("/p", "X-Requested-With: XMLHttpRequest", 
+  it("Two headers", function(){
+    methods.forEach(function(method){
+      app[method]("/p", "X-Requested-With: XMLHttpRequest", 
                     "X-Forwarded-For: 0.0.0.0",
                   function(req, res) {
 
@@ -17,13 +19,23 @@ describe('Header', function(){
         req.get("x-forwarded-for").should.equal("0.0.0.0")
         res.status(200).end();
       });
+    });
 
+    it('GET ', function(done){
       request(app)
         .get("/p")
         .set('X-Requested-With', 'XMLHttpRequest')
         .set("X-Forwarded-For", "0.0.0.0")
         .expect(200, done);
+    });
 
+    it('POST ', function(done){
+      request(app)
+        .post("/p")
+        .set('X-Requested-With', 'XMLHttpRequest')
+        .set("X-Forwarded-For", "0.0.0.0")
+        .expect(200, done);
+    });
   });
 
   it("One header", function(done){
@@ -38,7 +50,7 @@ describe('Header', function(){
         .expect(200, done);
   });
 
-  it("One header with whitespace", function(done){
+  it("Header with whitespace", function(done){
       request(app)
         .get("/p")
         .set('X-Requested-With', ' 	XMLHttpRequest')
--- test/params.js
+++ test/params.js
@@ -2,46 +2,60 @@ var express = require('express')
   , router = require("../index")
   , request = require('supertest')
   , chai = require("chai")
-  , should = chai.should();
+  , should = chai.should()
+  , methods = require("methods")
   
 var app = express();
 router.extend(app);
 
 describe("Router", function(){
-
-  it('GET /p?foo=bar&baz=quux', function(done){
-      app.get("/p?foo=bar&baz=quux", function(req, res){
+  before(function(){
+    methods.forEach(function(method){
+      app[method]("/p?foo=bar&baz=quux", function(req, res){
         req.param("foo").should.equal("bar")
         req.param("baz").should.equal("quux")
         res.status(200).end();
       });
 
-      request(app)
-        .get("/p?foo=bar&baz=quux")
-        .expect(200, done);
-    });
-
-  describe('Router param matcher', function(){
-    it('GET /p?foo=bar', function(done){
-      app.get("/p?foo=bar", function(req, res){
+      app[method]("/p?foo=bar", function(req, res){
         req.param("foo").should.equal("bar");
+        should.not.exist(req.param("baz"))
         res.status(200).end();
       });
 
+      app.get("/p?foo=", function(req, res){
+        var foo = req.param("foo");
+        foo.should.exist;
+        foo.should.not.equal("bar")
+        res.status(200).end();
+      });
+
+    });
+  });
+
+  it('GET /p?foo=bar&baz=quux', function(done){
+      request(app)
+        .get("/p?foo=bar&baz=quux")
+        .expect(200, done);
+  });
+
+  it('POST /p?foo=bar&baz=quux', function(done){
+      request(app)
+        .post("/p?foo=bar&baz=quux")
+        .expect(200, done);
+  });
+
+  describe('Router param matcher', function(){
+    it('GET /p?foo=bar', function(done){
       request(app)
         .get("/p?foo=bar")
         .expect(200, done);
     });
 
     it('GET /p?foo= any value', function(done){
-      app.get("/p?foo=", function(req, res){
-        res.status(200).end();
-      });
-
       request(app)
         .get("/p?foo=something")
         .expect(200, done);
     });
-
   });
 });
